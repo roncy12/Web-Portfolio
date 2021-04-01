d71c511df3b016f69c47e107e75f2ad81c8448c9
roncy12 <60778031+roncy12@users.noreply.github.com>
2021-04-01 20:04:47 +0800

    Fixing error

--- src/containers/App.js
+++ src/containers/App.js
@@ -36,6 +36,7 @@ const particlesOptions = {
   class App extends Component {
     render(){
         return (
+          
            <div className="App">
   
           
@@ -51,51 +52,51 @@ const particlesOptions = {
                         </Nav>
                         <Nav>
                           <Nav.Link href="#deets" className="text-light">
-                            <img src={github} alt="" class="icon-account" />
+                            <img src={github} alt="" className="icon-account" />
                           </Nav.Link>
                           <Nav.Link eventKey={2} href="#memes" className="text-light">
-                            <img src={email} alt="" class="icon-account" />
+                            <img src={email} alt="" className="icon-account" />
                           </Nav.Link>
 
                         </Nav>
                       </Navbar.Collapse>
                     </Navbar>
 
-                    <div id="hero" class="jumbotron">
-                       <div class="container">
-                       <h1 class="hero-title load-hidden" >
-                        Hi, my name is <span class="text-info">John Roncy Nava</span>
+                    <div id="hero" className="jumbotron">
+                       <div className="container">
+                       <h1 className="hero-title load-hidden" >
+                        Hi, my name is <span className="text-info">John Roncy Nava</span>
                        <br />
                        I'm the Developer you need.
                        </h1>
-                       <p class="hero-cta load-hidden" >
-                        <a class="cta-btn cta-btn--hero" href="#about">Hire me</a>
+                       <p className="hero-cta load-hidden" >
+                        <a className="cta-btn cta-btn--hero" href="#about">Hire me</a>
                        </p>
                        </div>
                     </div>
 
                
-                      <div class="container"><br />
-                        <h2 class="section-title text-uppercase text-light">
+                      <div className="container"><br />
+                        <h2 className="section-title text-uppercase text-light">
                           About me
                         </h2>
-                        <div class="row about-wrapper">
-                          <div class="col-md-6 col-sm-12">
-                            <div class="about-wrapper__image">
-                             <Tilt className="Tilt" options={{ max : 25 }}  >
-                              <img
-                                class="img-fluid rounded shadow-lg"
-                                height="auto"
-                                width="300px"
-                                src={profile}
-                                alt=""
-                              />
-                              </Tilt>
+                        <div className="row about-wrapper">
+                          <div className="col-md-6 col-sm-12">
+                            <div className="about-wrapper__image">
+                               <Tilt className="Tilt" options={{ max : 25 }}  >
+                                <img
+                                  className="img-fluid rounded shadow-lg"
+                                  height="auto"
+                                  width="300px"
+                                  src={profile}
+                                  alt=""
+                                />
+                                </Tilt>
                             </div>
                           </div>
-                          <div class="col-md-6 col-sm-12">                          
-                              <div class="about-wrapper__info">
-                                <p class="about-wrapper__info-text text-justify text-white">
+                          <div className="col-md-6 col-sm-12">                          
+                              <div className="about-wrapper__info">
+                                <p className="about-wrapper__info-text text-justify text-white">
                                         Personal Skills  <br />
                                         • Skilled in designing user interfaces and websites <br />
                                         • Skilled in logical, analytical and computational skills. <br />
@@ -104,7 +105,7 @@ const particlesOptions = {
                                         • I used Drupal 8 on my website Project in our Thesis.
 
                                   </p>
-                                 <p class="about-wrapper__info-text text-justify text-white">
+                                 <p className="about-wrapper__info-text text-justify text-white">
                                       Technical <br />
                                             • Programming: Java, C++, Vb.net<br />
                                             • Database: Microsoft SQL<br />
@@ -112,8 +113,8 @@ const particlesOptions = {
                                             • Operating System: Windows
 
                                     </p>
-                                        <span class="d-flex mt-3">
-                                          <a target="_blank" class="cta-btn cta-btn--resume" href="#!">
+                                        <span className="d-flex mt-3">
+                                          <a target="_blank" className="cta-btn cta-btn--resume" href="#!">
                                             View Resume
                                           </a>
                                         </span>
@@ -124,37 +125,36 @@ const particlesOptions = {
               
      
     
-                    <div class="container">
-                      <div class="project-wrapper">
-                        <h2 class="section-title dark-blue-text text-light">
+                    <div className="container">
+                      <div className="project-wrapper">
+                        <h2 className="section-title dark-blue-text text-light">
                           Projects
                         </h2>
           
-                        <div class="row">
-                          <div class="col-lg-4 col-sm-12">
-                            <div class="project-wrapper__text">
-                              <h3 class="project-wrapper__text-title text-light">Robofriends</h3>
+                        <div className="row">
+                          <div className="col-lg-4 col-sm-12">
+                            <div className="project-wrapper__text">
+                              <h3 className="project-wrapper__text-title text-light">Robofriends</h3>
                               <div>
-                                <p class="mb-4 text-light">
+                                <p className="mb-4 text-light">
                                       First project created using React JS and Redux, 
                                       helps to understand a "search by name of robot" technique (project created for Udemy Web zero to mastery course)
                                   </p>
-                              </div>
-                              
-                              <a target="_blank" rel="noreferrer"  class="cta-btn cta-btn--hero" href="https://roncy12.github.io/Updated-Robofriends/">
+                              </div>                    
+                              <a target="_blank" rel="noreferrer"  className="cta-btn cta-btn--hero" href="https://roncy12.github.io/Updated-Robofriends/">
                                 See Live
                               </a>
-                              <a target="_blank" rel="noreferrer" class="cta-btn text-color-main" href="https://github.com/roncy12/Updated-Robofriends">
+                              <a target="_blank" rel="noreferrer" className="cta-btn text-color-main" href="https://github.com/roncy12/Updated-Robofriends">
                                 Source Code
                               </a>
                             </div>
                           </div>
-                          <div class="col-lg-8 col-sm-12">
-                            <div class="project-wrapper__image">
+                          <div className="col-lg-8 col-sm-12">
+                            <div className="project-wrapper__image">
                             <a href="https://roncy12.github.io/Updated-Robofriends/" rel="noreferrer" target="_blank">
-                                <div data-tilt class="thumbnail rounded">
+                                <div data-tilt className="thumbnail rounded">
                                 <Tilt className="Tilt" options={{ max : 25 }}  >
-                                 <img class="img-fluid" alt="" src={Robofriends} />
+                                 <img className="img-fluid" alt="" src={Robofriends} />
                                 </Tilt>
                                 </div>
                             </a>
@@ -164,89 +164,92 @@ const particlesOptions = {
                           </div>
                         </div>
               
-                    <div class="row">
-                      <div class="col-lg-4 col-sm-12">
-                        <div class="project-wrapper__text">
-                          <h3 class="project-wrapper__text-title text-light">Face Recognition Web App in TypeScript with React, Express, Node, PostgreSQL</h3>
-                          <div>
-                            <p class="mb-4 text-light">
-                                 The full stack face recognition app I built with React, Express, Node and PostgreSQL in TypeScript. This app was inspired by the excellent Udemy course The Complete Web Developer in 2020: Zero to Mastery by Andrei Neagoie.
-                            </p>
-                          </div>
-                          <a target="_blank" rel="noreferrer" class="cta-btn cta-btn--hero" href="https://detect-smart-brain.herokuapp.com/">
-                            See Live
-                          </a>
-                          <a target="_blank" rel="noreferrer" class="cta-btn text-color-main" href="https://github.com/roncy12/facerecognitionbrain/tree/master">
-                            Source Code
-                          </a>
-                        </div>
-                      </div>
-                      <div class="col-lg-8 col-sm-12">
-                        <div class="project-wrapper__image">
-                          <a href="https://detect-smart-brain.herokuapp.com/" rel="noreferrer" target="_blank">
-                            <div data-tilt class="thumbnail rounded">
-                             <Tilt className="Tilt" options={{ max : 25 }}  >
-                              <img class="img-fluid" alt="" src={FaceDetect} />
-                              </Tilt>
+                    <div className="row">
+                        <div className="col-lg-4 col-sm-12">
+                          <div className="project-wrapper__text">
+                            <h3 className="project-wrapper__text-title text-light">Face Recognition Web App in TypeScript with React, Express, Node, PostgreSQL</h3>
+                            <div>
+                              <p className="mb-4 text-light">
+                                   The full stack face recognition app I built with React, Express, Node and PostgreSQL in TypeScript. This app was inspired by the excellent Udemy course The Complete Web Developer in 2020: Zero to Mastery by Andrei Neagoie.
+                              </p>
                             </div>
-                          </a>
+                            <a target="_blank" rel="noreferrer" className="cta-btn cta-btn--hero" href="https://detect-smart-brain.herokuapp.com/">
+                              See Live
+                            </a>
+                            <a target="_blank" rel="noreferrer" className="cta-btn text-color-main" href="https://github.com/roncy12/facerecognitionbrain/tree/master">
+                              Source Code
+                            </a>
+                          </div>
                         </div>
-                      </div>
-                    </div>
-                
-                     <footer class="footer navbar-static-bottom">
 
-                      <div class="container">
-                        <a href="#top" class="back-to-top">
-                          <i class="fa fa-angle-up fa-2x" aria-hidden="true"></i>
-                        </a>
-                        <div class="social-links">
-                          <a href="#!" target="_blank">
-                            <i class="fa fa-twitter fa-inverse"></i>
-                          </a>
-                          <a href="#!" target="_blank">
-                            <i class="fa fa-codepen fa-inverse"></i>
-                          </a>
-                          <a href="#!" target="_blank">
-                            <i class="fa fa-linkedin fa-inverse"></i>
-                          </a>
-                          <a href="#!" target="_blank">
-                            <i class="fa fa-github fa-inverse"></i>
-                          </a>
+                        <div className="col-lg-8 col-sm-12">
+                          <div className="project-wrapper__image">
+                            <a href="https://detect-smart-brain.herokuapp.com/" rel="noreferrer" target="_blank">
+                              <div data-tilt className="thumbnail rounded">
+                               <Tilt className="Tilt" options={{ max : 25 }}  >
+                                <img className="img-fluid" alt="" src={FaceDetect} />
+                                </Tilt>
+                              </div>
+                            </a>
+                          </div>
                         </div>
+                    </div>
+                    </div>
+                    </div>
+       
+
+                    <footer className="footer navbar-static-bottom">
+
+                      <div className="container">
+
+                          <a href="#top" className="back-to-top">
+                            <i className="fa fa-angle-up fa-2x" aria-hidden="true"></i>
+                          </a>
+
+                          <div className="social-links">
+                              <a href="#!" target="_blank">
+                                <i className="fa fa-twitter fa-inverse"></i>
+                              </a>
+                              <a href="#!" target="_blank">
+                                <i className="fa fa-codepen fa-inverse"></i>
+                              </a>
+                              <a href="#!" target="_blank">
+                                <i className="fa fa-linkedin fa-inverse"></i>
+                              </a>
+                              <a href="#!" target="_blank">
+                                <i className="fa fa-github fa-inverse"></i>
+                              </a>
+                          </div>
 
                         <hr />
 
                   
-                        <p class="footer__text text-light">
-                           <h2 class="section-title text-light">
+      
+                           <h2 className="section-title text-light">
                             Contact
                           </h2>
-                         <div class="contact-wrapper">
-                            <p class="contact-wrapper__text text-light">
+                         <div className="contact-wrapper">
+                            <p className="contact-wrapper__text text-light">
                              jroncynavs12@gmail.com<br />
                              +63-935-853-4264
                             </p>
                             <a
                               target="_blank"
-                              class="cta-btn cta-btn--resume"
+                              className="cta-btn cta-btn--resume"
                               href="jroncynavs12@gmail.com"
                               >Call to Action</a>
                           </div>
-                          © 2021- Template developed by <a href="https://github.com/cobidev" rel="noreferrer" className="text-light" target="_blank">John Roncy Nava</a>
+                          <a href="https://github.com/cobidev" rel="noreferrer" className="text-light" target="_blank"> © 2021- Template developed by John Roncy Nava</a>
                      
-                        </p>
+             
 
       
                       </div>
                     </footer>
 
 
-                    </div>
-                        </div>                      
+                           
           
-
-                
                 <Particles className='particles'
                   params={particlesOptions}            /> 
                  
